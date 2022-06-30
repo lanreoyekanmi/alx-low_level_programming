201ab1c2b4c7d5e8bee1824a46be6ab46fe3eeb3
Lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2022-06-30 05:09:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * master - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
+int master(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
