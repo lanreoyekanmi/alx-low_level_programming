c3dffb7cce13bd07252c4d0fd4499bc269a0e5b2
Lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2022-06-30 10:09:09 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - to be called
- * return:0 for the int
- */
-
+* main - to be called
+* return:0 for the int
+*/
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
